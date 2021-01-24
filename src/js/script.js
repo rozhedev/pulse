4cf8684c7b9f200ec6407fb65e671eca846d53a1
bohdan443 <bohdan443@gmail.com>
2021-01-24 11:12:30 +0200

    create consultation section & slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 // @@include('alert.js');
+
+$(document).ready(function () {
+    $('.slider__wrapper').slick({
+        speed: 700,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/catalog/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/catalog/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false,
+                    dots: true, 
+                }
+            }
+        ]
+    });
+});
