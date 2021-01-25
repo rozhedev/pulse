ab52b601b139c1e46443920ca88b30d8f0f56399
bohdan443 <bohdan443@gmail.com>
2021-01-25 13:51:47 +0200

    Create catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -12,9 +12,28 @@ $(document).ready(function () {
                 breakpoint: 769,
                 settings: {
                     arrows: false,
-                    dots: true, 
+                    dots: true,
                 }
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function () {
+        $(this)
+            .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+    });
+
+    function toggleClass(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__main').eq(i).toggleClass('catalog-item__main--active');
+                $('.catalog-item__more').eq(i).toggleClass('catalog-item__more--active');
+            })
+        })
+    };
+
+    toggleClass('.catalog-item__link');
+    toggleClass('.catalog-item__back');
 });
